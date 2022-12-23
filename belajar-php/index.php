27cabb0fdfe090cff36ce5492ed4ee0dafd22fa7
rahmanrfaaa <rahmafadillah184@gmail.com>
2022-12-23 10:25:39 +0700

    first commit

---
+++ belajar-php/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    echo "Hello World!<br>";
+
+    $nama = "Rahma Nur Fadillah";
+    $umur = 20;
+
+    echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+    $namaAdik = "Rafi Ali";
+    $umurAdik = "13";
+
+    echo "Nama adik saya <strong>$namaAdik</strong>, saya berusia $umurAdik tahun.<br>";
+
+    $selisihUmur = $umur - $umurAdik;
+    echo "selisih usia saya dan adik saya adalah $selisihUmur tahun."
+
+?>
